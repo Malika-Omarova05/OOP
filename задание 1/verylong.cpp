253163f71d42c472326eacd8323fd9821e1b7415
Malika-Omarova05 <malika.omarova.r@gmail.com>
2025-04-25 12:17:47 +0600

    Add files via upload

---
+++ задание 1/verylong.cpp
@@ -0,0 +1,27 @@
+#include "verylong.h"
+#include <iostream>
+using namespace std;
+VeryLong::VeryLong() :a(0) {}
+VeryLong::VeryLong(long double a1) :a(a1) {}
+
+void VeryLong::getdata() {
+	cout << "Введите число -> "; cin >> a;
+}
+
+VeryLong VeryLong::operator-(const VeryLong& other){
+	return VeryLong(a - other.a);
+}
+VeryLong VeryLong::operator+(const VeryLong& other) {
+	return VeryLong(a + other.a);
+}
+VeryLong VeryLong::operator*(const VeryLong& other) {
+	return VeryLong(a * other.a);
+}
+VeryLong VeryLong::operator/(const VeryLong& other) {
+	if (other.a != 0) return VeryLong(a / other.a);
+	else cout << "На ноль делить нельзя!" << endl;
+}
+void VeryLong::putdata() {
+	cout << a;
+}
+
---
+++ задание 1/verylong.h
@@ -0,0 +1,41 @@
+#pragma once 
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class VeryLong {
+private:
+	long double a;
+public:
+	VeryLong();
+	VeryLong(long double a1);
+	void getdata();
+	VeryLong operator-(const VeryLong& other);
+	VeryLong operator+(const VeryLong& other);
+	VeryLong operator*(const VeryLong& other);
+	VeryLong operator/(const VeryLong& other);
+	void putdata();
+};
+
+//class VeryLong {
+//private:
+//    string number;
+//    bool isNegative;
+//
+//    // Вспомогательные методы
+//    string subtractStrings(const string& num1, const string& num2) const;
+//    string divideStrings(const string& num1, const string& num2) const;
+//
+//public:
+//    VeryLong(string num = "0");
+//
+//    // Перегрузка операторов
+//    VeryLong operator-(const VeryLong& other);
+//    VeryLong operator/(const VeryLong& other);
+//
+//    void display() const;
+//};
+//
+//#endif
